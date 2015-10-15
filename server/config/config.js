57231e58a44ac5693fe84935c06595337feadc2a
Amin sheikhi <aminsheikhi@gmail.com>
2015-10-16 02:07:36 +0330

    Changed db Name added code sample for auth and removed userApi.js

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ var rootPath = path.normalize(__dirname + '/../../');
 module.exports = {
     development: {
         rootPath: rootPath,
-        db: 'mongodb://localhost:27017/eMarket',
+        db: 'mongodb://localhost:27017/steamIran',
         port: process.env.PORT || 3030
     },
     production: {
