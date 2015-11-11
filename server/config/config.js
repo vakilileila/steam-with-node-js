40d022eb384f62b80f692819bf690442bcdd4bd6
vakilileila <vakililaila@yahoo.com>
2015-11-11 16:45:59 +0330

    connection in development

--- server/config/config.js
+++ server/config/config.js
@@ -5,13 +5,13 @@ var rootPath = path.normalize(__dirname + '/../../');
 module.exports = {
     development: {
         rootPath: rootPath,
-        db: 'mongodb://localhost:27017/dbDota2',
-        port: process.env.PORT || 3030
+        db: 'mongodb://iransteamdb:l-8611268296@ds053164.mongolab.com:53164/iransteamdb'
+
     },
 
 
     production: {
-        db: ' mongodb://iransteamdb:l-8611268296@ds053164.mongolab.com:53164/iransteamdb',
+        db: 'mongodb://iransteamdb:l-8611268296@ds053164.mongolab.com:53164/iransteamdb',
         root: rootPath,
         app: {
             name: 'MEAN - A Modern Stack - Production'
