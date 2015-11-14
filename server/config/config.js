c2c7f07664c50ac1c16cd28874acb95714b91e5e
vakilileila <vakililaila@yahoo.com>
2015-11-14 10:01:09 +0330

    make it better

--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,14 @@
 var path = require('path');
 var rootPath = path.normalize(__dirname + '/../../');
 
-
 module.exports = {
     development: {
         rootPath: rootPath,
-        db: 'mongodb://dbadmin:123456@ds053764.mongolab.com:53764/dbiransteam'
-
+        db: 'mongodb://admin:admin@ds053784.mongolab.com:53784/dbiransteam',
+        port: process.env.PORT || 3030
     },
     production: {
-        db: 'mongodb://dbadmin:123456@ds053764.mongolab.com:53764/dbiransteam',
+        db: 'mongodb://admin:admin@ds053784.mongolab.com:53784/dbiransteam',
         root: rootPath,
         app: {
             name: 'MEAN - A Modern Stack - Production'
